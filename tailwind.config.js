5c17b39ed58b5cc4995e2eef0861342c14120f45
Clemente Silva <clementesilvavicuna@gmail.com>
2024-11-06 08:42:09 -0300

    denuevo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
 const { nextui } = require("@nextui-org/react");
+const withMT = require("@material-tailwind/react/utils/withMT");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
-    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      // Agregar configuración personalizada para el carrusel
       fontFamily: {
         slick: ["slick"],
       },
       colors: {
         slick: {
+          // Personaliza los colores del carrusel según sea necesario
           bg: "white",
           text: "black",
         },
@@ -24,4 +28,5 @@ module.exports = {
   },
   darkMode: "class",
   plugins: [nextui()],
+  tailwindcss: {},
 };
